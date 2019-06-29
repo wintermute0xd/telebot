b3048f35a3ec1986b17c146e70e03786badc2c26
v1101 <v1101@test.local>
2019-06-29 15:38:59 +0300

    working version

--- TeleBotHandler.py
+++ TeleBotHandler.py
@@ -2,12 +2,18 @@ import requests
 import datetime
 import logging
 
+'''
+Class for telegram bot.
+Can get updates from Telegram API and make response in depends of chat text message.
+'''
+
 
 class TeleBotHandler:
     def __init__(self, token):
         self.token = token
         self.api_url = 'https://api.telegram.org/bot' + token + '/'
 
+    # gets updates from telegram api by GET and returning update json object
     def get_updates(self, offset=None, timeout=30):
         logging.info('Started')
         method = 'getUpdates'
@@ -19,3 +25,31 @@ class TeleBotHandler:
             return None
         result_json = resp.json()['result']
         return result_json
+
+    def request_handler(self, update):
+        hello_text = ['hello', 'good day', 'good morning', 'good evening', 'привет', 'привіт', 'добрый день', 'доброго дня']
+
+        now = datetime.datetime.now()
+        cur_hour = now.hour
+        in_text = update['message']['text']
+        chat_name = update['message']['chat']['first_name']
+        bot_response = 'I don\'t understand you'
+
+        if in_text.lower() in hello_text:
+            if 4 <= cur_hour <= 12:
+                bot_response = 'Good morning'
+            elif 12 < cur_hour <= 17:
+                bot_response = 'Good day'
+            elif 17 < cur_hour <= 22:
+                bot_response = 'Good evening'
+            elif 22 < cur_hour < 4:
+                bot_response = 'Good night'
+
+        return bot_response + ', ' + chat_name
+
+    def send_message(self, chat_id, out_text):
+        params = {'chat_id': chat_id, 'text': out_text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
--- telebot_main.py
+++ telebot_main.py
@@ -11,8 +11,20 @@ def main():
 
     with open('config.json', 'r') as cfg_file:
         data = json.load(cfg_file)
-    bot = TeleBotHandler.TeleBotHandler(data['token'])
-    bot.get_updates()
+    new_offset = None
+
+    while True:
+        bot = TeleBotHandler.TeleBotHandler(data['token'])
+        updates_list = bot.get_updates(new_offset)
+        for update in updates_list:
+            cur_update_id = update['update_id']
+            cur_chat_id = update['message']['chat']['id']
+
+            bot_response = bot.request_handler(update)
+            bot.send_message(cur_chat_id, bot_response)
+            new_offset = cur_update_id + 1
+
+        print(new_offset, '\n')
 
 
 if __name__ == '__main__':
